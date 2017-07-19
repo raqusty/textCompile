ae29d8384e141f38964a057612f047013aa4ee97
linzehao <linzehao@aipai.com>
2017-07-19 15:25:46 +0800

    测试

---
+++ mylibrary/src/main/java/com/example/mylibrary/test.java
@@ -0,0 +1,19 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/7/19.
+ */
+
+public class test {
+
+    public test() {
+
+    }
+
+    public void print() {
+        Log.i("linzehao", "ssssss");
+    }
+
+}
